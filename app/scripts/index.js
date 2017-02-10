fa217cbf28975c2297c4123feb5387d2fdc06713
evan miller <emille4@clemson.edu>
2017-02-09 19:42:05 -0500

    setup splash and switch to fight page

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,40 @@
 
+var $ =  require('jquery');
+var models = require('../scripts/models');
+
+var splashTemplate = require('../templates/splash.hbs');
+var gameTemplate = require('../templates/fight.hbs');
+
+// $('#choose-character').append(splashTemplate());
+  $('#choose-character').html(splashTemplate());
+
+  $('.start-game').click(function(event){
+    $('#choose-character').html(gameTemplate());
+  })
+
+
+
+
+
+
+
+
+//make a page to select hero type from dropdown
+
+//show enemies list (allow to select enemy if have enough time/dont have to)
+
+//float in players (stretch goal)
+
+//say "who ever is computer" wants to fight
+
+//user attack
+
+//say what user did attack was (stretch goal)
+
+//show damage to "computer" in health bar
+
+//computer attacks automatically
+
+//show damage to user in health bar
+
+//set whoevers health is at 0 looses
---
+++ app/scripts/models.js
@@ -0,0 +1,30 @@
+// var $ = require ('jquery');
+// var index = require('../scripts/index.js')
+
+// function Character(){
+//
+// }
+//
+// /*
+// *Hero prototype of Character
+// */
+//
+// function Heroes(){
+//
+// }
+// Hero.prototype = new Character ();
+//
+// var bart = new Hero({
+//
+// })
+//
+// /*
+// *villians prototype of Character
+// */
+//
+// function Villian(){
+//
+// }
+// Villian.prototype = new Character(){
+//
+// }
