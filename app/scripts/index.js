0604fe5afdd2c9b77f441bf2c594fe117140de74
evan miller <emille4@clemson.edu>
2017-02-12 12:31:57 -0500

    added comments to clarify things for group work

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,27 +3,32 @@ var $ =  require('jquery');
 var models = require('./models');
 var Handlebars = require('handlebars');
 var _ = require('underscore');
-
+//these variables are the splash.hbs file and the fight.hbs files
 var splashTemplate = require('../templates/splash.hbs');
 var gameTemplate = require('../templates/fight.hbs');
 
+//selects the villains from models.js
 var homer = models.homer;
 var krusty = models.krusty;
 var nelson = models.nelson;
 var randomArray = [homer, krusty, nelson];
+//chooses random villain and compiles to handlebars template
 var selectedVillain = randomArray[_.random(randomArray.length-1)];
 var villainSource = $('#villain-template').html();
 var villainTemplate = Handlebars.compile(villainSource);
 
-//make a page to select hero type from dropdown
+//puts the splash.hbs in the choose-character index.html div
 $('#choose-character').html(splashTemplate());
 
+//makes all the things happen in the function when click start game
 $('.start-game').on('click', function(event){
   event.preventDefault();
-
+//sets selectedHero to the hero thats selected value
 var selectedHero = $('.hero-select').val();// set selectedHero
-  // var myVillian = $('.villian-select').val();
+//empties splash.hbs template from the choose character div in index.html
 $('#choose-character').empty();
+//fills the choose character div in index.html with selectedHero variable from above. and gets the info from models.js constructors
 $('#choose-character').append(gameTemplate(models[selectedHero]));// currentHero = models[selectedHero]
+//fills the .villian-input div in index.html with compiled villainTemplate above with the randomly selected villain from selectedVillain variable
 $(".villain-input").html(villainTemplate(selectedVillain));
   });
